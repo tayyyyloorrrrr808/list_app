cdf6e4a77e0c040db7603eaa2ba949791f81e85c
Taylor <tmoody21@punahou.edu>
2016-06-27 14:26:19 -1000

    tried to do the challenge
    js challenge

---
+++ js/index.js
@@ -0,0 +1,23 @@
+var colors = [
+	"red",
+	"orange",
+	"yellow",
+	"green",
+	"indigo",
+	"violet"
+	];
+
+var count = 0;
+
+var cycle = function(array) {
+	console.log(array[count]);
+	if (count == array.length-1) {
+		count = 0;
+	}else{
+
+	};
+};
+
+var display = function(){
+	cycle(colors);
+};
